ea6fd8db50607773e4bba548290288e434a03fa3
Luis Huanca <luanhuancam@gmail.com>
2024-05-19 20:50:34 -0400

    funcion busqueda entre pacientes y sus pantallas

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-final Color colorPrimario=Color(0xFF02457A);
-final Color colorSecundario=Color(0xFF018ABE);
-final Color colorTerciario=Color(0xFF97CADB);
-final Color colorCuarto=Color(0xFFD6E8EE);
+const Color colorPrimario=Color(0xFF02457A);
+const Color colorSecundario=Color(0xFF018ABE);
+const Color colorTerciario=Color(0xFF97CADB);
+const Color colorCuarto=Color(0xFFD6E8EE);
