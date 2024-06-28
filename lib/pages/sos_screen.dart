83649a97da5ca3645c16513b1daf3a5919435981
Ismael Rivera <ismael.rivera.work@gmail.com>
2024-06-27 21:03:09 -0400

    Sos request
    A;adida la funcion de solicitar sos

--- lib/pages/sos_screen.dart
+++ lib/pages/sos_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'navBar.dart'; // Asegúrate de tener la importación correcta de tu NavBar
 
 class SOSSplashScreen extends StatelessWidget {
@@ -6,36 +7,67 @@ class SOSSplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red, // Fondo transparente
-        elevation: 0, // Sin sombra
+        backgroundColor: Colors.red,
+        elevation: 0,
         iconTheme: IconThemeData(
-          color: Colors.black, // Color del icono de menú
+          color: Colors.black,
         ),
         leading: Builder(
           builder: (BuildContext context) {
             return IconButton(
               icon: Icon(Icons.menu),
               onPressed: () {
-                Scaffold.of(context).openDrawer(); // Abre el drawer al presionar el ícono del menú
+                Scaffold.of(context).openDrawer();
               },
             );
           },
         ),
       ),
-      drawer: NavbarOptions(), // Aquí se añade el drawer
+      drawer: NavbarOptions(),
       body: Container(
-        color: Colors.red, // Fondo rojo
+        color: Colors.red,
         child: Center(
-          child: Text(
-            'SOS',
-            style: TextStyle(
-              fontSize: 72, // Tamaño de fuente grande
-              color: Colors.white, // Texto blanco
-              fontWeight: FontWeight.bold, // Texto en negrita
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'SOS',
+                style: TextStyle(
+                  fontSize: 72,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _sendSOS(context),
+                child: Text('Enviar SOS'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.red, backgroundColor: Colors.white, // Texto rojo
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void _sendSOS(BuildContext context) async {
+    try {
+      await FirebaseFirestore.instance.collection('SOS').add({
+        'sos_signal': 1,
+        'timestamp': FieldValue.serverTimestamp(), // Añade un timestamp para referencia
+      });
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('SOS enviado con éxito'),
+      ));
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error al enviar SOS: $e'),
+      ));
+    }
   }
 }
